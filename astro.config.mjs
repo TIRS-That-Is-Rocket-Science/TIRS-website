4aac6f9b41a89c458a9fc1bf3c86e3f74e631122
Mathias Boudreau <mabo1123@ulaval.ca>
2026-03-11 09:55:51 -0400

    Ajout starwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 
 export default defineConfig({
-  integrations: [mdx(), react()],
+	integrations: [mdx(), react()],
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
